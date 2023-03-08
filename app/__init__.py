35554b1ccb0deea6225f299a3ebbed66475901c4
root <root@Desktop-B1>
2023-03-07 17:20:19 -0800

    changed init

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<h1>Hello World!</h1>"
+
+@app.get('/aboutme')
+def aboutme():
+    me = {'first_name': 'John', 'last_name': 'Garcia', 'hobby': 'Gaming'}
+    return (me)
